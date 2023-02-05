675f145835c6cd59aee5e41c001d559ae14eef13
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-02-05 17:22:22 +0200

    Linked List can now push object in the list

--- Linked List.playground/Contents.swift
+++ Linked List.playground/Contents.swift
@@ -1,12 +1,32 @@
 import UIKit
 
+struct LinkedList<Value>{
+    
+    var head: Node<Value>?
+    var tail: Node<Value>?
+    
+    var isEmpty: Bool{
+        return head == nil
+    }
+    
+     mutating func push(_ value: Value){
+        /// push the new value in the place of the head
+        /// What ever was previous becomes the next node
+        head  = Node(value: value, next: head)
+        if tail == nil {
+            tail = head
+        }
+    }
+    public init() {}
+}
+
 /// Generic implementation where the value can be anything
-class Node<Value>{
+ class Node<Value>{
     
     var value: Value
     var next: Node? /// There might not be a next node
     
-    init(value: Value, next: Node? = nil) {
+    public init(value: Value, next: Node? = nil) {
         self.value = value
         self.next = next
     }
@@ -14,7 +34,7 @@ class Node<Value>{
 
 ///Printing the values in a readable way
 extension Node: CustomStringConvertible{
-    var description: String {
+    public var description: String {
         guard let next = next else {
             return "\(value)"
         }
@@ -22,12 +42,9 @@ extension Node: CustomStringConvertible{
     }
 }
 
-///IN USE
-let node1 = Node(value: 1)
-let node2 = Node(value: 2)
-let node3 = Node(value: 4)
+var list = LinkedList<Int>()
+list.push(2)
+list.push(3)
+list.push(22)
 
-node1.next = node2
-node2.next = node3
-print(node1)
-print(node1.next)
+print(list)
