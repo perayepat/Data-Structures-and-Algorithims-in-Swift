6334be6e2cdd9dc4ca81eb2bf48aca3ad8d199fa
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-02-12 13:19:11 +0200

    Using the recursive function to find the power of a number

--- Recursion.playground/Contents.swift
+++ Recursion.playground/Contents.swift
@@ -1,7 +1,6 @@
 import UIKit
 
 func factorial(number :Int) -> Int {
-    
     //base case
     if number == 0 {
         return 1
@@ -12,3 +11,14 @@ func factorial(number :Int) -> Int {
 
 let result = factorial(number: 6)
 
+//2^3
+func power(number :Int, n :Int) -> Int{
+    //base recurcisive case
+    if n == 0 {
+        return 1
+    } else {
+        return number * power(number: number, n: n - 1)
+    }
+}
+
+power(number: 6, n: 2)
