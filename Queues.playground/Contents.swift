5e58506fb2f9541810f8901d7418e2cdbd70e9ed
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-02-12 12:14:07 +0200

    Queue implementation done

--- Queues.playground/Contents.swift
+++ Queues.playground/Contents.swift
@@ -1,3 +1,47 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct Queue<T>{
+    
+    var array :[T] = []
+    
+    init () {
+    }
+    
+    var isEmpty: Bool {
+        ///Utililzing the power of the array
+        return array.isEmpty
+    }
+    
+    ///Returns the first item but doens't remove it
+    var peek : T? {
+        return array.first
+    }
+    
+    mutating func enqueue(_ element: T) -> Bool{
+        array.append(element)
+        return true
+    }
+    
+    mutating func dequeue() -> T? {
+        return isEmpty ? nil : array.removeFirst()
+    }
+}
+
+extension Queue: CustomStringConvertible {
+    var description: String{
+        return String(describing: array)
+    }
+}
+
+var queue = Queue<Int>()
+queue.enqueue(10)
+queue.enqueue(23)
+queue.enqueue(25)
+queue.enqueue(30)
+queue.enqueue(20)
+
+print(queue)
+queue.dequeue()
+
+print(queue)
+print(queue.peek)
