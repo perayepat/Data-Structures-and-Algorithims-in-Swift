8c5348d61832839f794026dcf560f369fbe738e3
Mphathi Maapola <perrierpat@icloud.com>
2023-02-18 11:18:34 +0200

    Contains method is being

--- BinarySearchTree.playground/Contents.swift
+++ BinarySearchTree.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var bst = BinarySearchTree<Int>()
+
+bst.insert(3)
+bst.insert(1)
+bst.insert(4)
+bst.insert(0)
+bst.insert(2)
+bst.insert(5)
+
+//print(bst.contains(5))
--- BinarySearchTree.playground/Sources/BinarySearchTree.swift
+++ BinarySearchTree.playground/Sources/BinarySearchTree.swift
@@ -1,12 +1,12 @@
 import Foundation
 
-class BinaryNode<Element>{
+public class BinaryNode<Element>{
     
-    var value: Element
-    var leftChild: BinaryNode?
-    var rightChild: BinaryNode?
+    public var value: Element
+    public var leftChild: BinaryNode?
+    public var rightChild: BinaryNode?
     
-    init(value: Element) {
+    public init(value: Element) {
         self.value = value
     }
 }
@@ -26,25 +26,63 @@ extension BinaryNode: CustomStringConvertible {
             return root + "\(node.value)\n"
         }
         return diagram(for: node.rightChild,
-                       top + " ", top + "┌──", top + "│ ")
+                       top + " ",
+                       top + "┌──",
+                       top + "│ ")
     } }
 
-struct BinarySearchTree<Element: Comparable>{
+public struct BinarySearchTree<Element: Comparable>{
     
-    private(set) var root :BinaryNode<Element>
+    private(set) var root :BinaryNode<Element>?
+    public  init() {}
     
-    init() {}
 }
 
 extension BinarySearchTree: CustomStringConvertible {
     
-    var description: String {
-        
+    public var description: String {
         guard let root = root else { return "Empty tree" }
         return String(describing: root)
-        
     }
     
 }
 
-
+extension BinarySearchTree{
+    ///Because it's a struct we have to use a mutating property
+    public mutating func insert(_ value: Element){
+        root = insert(from: root, value: value)
+    }
+    
+    
+    private func insert(from node: BinaryNode<Element>?, value: Element) -> BinaryNode<Element>{
+        /// If there's no binary node add the new node
+        guard let node = node else {return BinaryNode(value: value)}
+        
+        //Using recusrsion this will keep on being called until the base case if fulfilled
+        if value < node.value{
+            node.leftChild = insert(from: node.leftChild, value: value)
+        } else {
+            node.rightChild = insert(from: node.rightChild, value: value)
+        }
+        
+        return node
+    }
+    
+    public func contains(_ value: Element) -> Bool{
+        var current = root
+        
+        while let node = current {
+            if node.value == value{
+                return true
+            }
+            
+            ///This is using recursion to check the magnitude of the node if it's lesser or greater than and using that to traverse through the binary tree
+            if value < node.value{
+                current = node.leftChild
+            } else {
+                current = node.rightChild
+            }
+        }
+        return false
+    }
+}
