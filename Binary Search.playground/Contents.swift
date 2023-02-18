ec8fb1a3a2e4222d84d64ead25d76a1f5bdaf611
Mphathi Maapola <perrierpat@icloud.com>
2023-02-18 11:59:20 +0200

    Binary Search algorithm added as an extension to the array

--- Binary Search.playground/Contents.swift
+++ Binary Search.playground/Contents.swift
@@ -1,25 +1,32 @@
 import UIKit
 
 let numbers = [1,5,15,17,19,22,24,31,105,150]
-var lowerBound = 0
-var upperBound = numbers.count - 1
-var middle = 0
-var found = false
-var searchedValue = 1
 
-while(lowerBound <= upperBound){
-    
-    //find the middle of the array
-    middle = (lowerBound + upperBound) / 2
-    
-    if(numbers[middle] == searchedValue){
-        found = true
-        break
-    } else if(numbers[middle] < searchedValue) {
-       lowerBound = middle + 1
-    } else {
-        upperBound = middle - 1
+extension Array where Iterator.Element == Int{
+ 
+     func containsUsingBinarySearch(_ value: Int) -> Bool{
+        var lowerBound = 0
+        var upperBound = numbers.count - 1
+        var middle = 0
+        var found = false
+
+        while(lowerBound <= upperBound){
+            
+            //find the middle of the array
+            middle = (lowerBound + upperBound) / 2
+            
+            if(numbers[middle] == value){
+                found = true
+                break
+            } else if(numbers[middle] < value) {
+               lowerBound = middle + 1
+            } else {
+                upperBound = middle - 1
+            }
+        }
+        
+        return found
     }
 }
 
-print(found)
+print(numbers.containsUsingBinarySearch(31))
