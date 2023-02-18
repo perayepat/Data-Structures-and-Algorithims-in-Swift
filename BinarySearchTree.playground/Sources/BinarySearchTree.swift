8c12aa4688987cc704a266384bb295f665f8b7a7
Mphathi Maapola <perrierpat@icloud.com>
2023-02-18 10:43:10 +0200

    Code added for display purposes

--- BinarySearchTree.playground/Sources/BinarySearchTree.swift
+++ BinarySearchTree.playground/Sources/BinarySearchTree.swift
@@ -4,26 +4,47 @@ class BinaryNode<Element>{
     
     var value: Element
     var leftChild: BinaryNode?
-    var righttChild: BinaryNode?
+    var rightChild: BinaryNode?
     
     init(value: Element) {
         self.value = value
     }
 }
 
-struct BinarySeachTree<Element: Comparable>{
+extension BinaryNode: CustomStringConvertible {
+    public var description: String {
+        return diagram(for: self)
+    }
+    private func diagram(for node: BinaryNode?,
+                         _ top: String = "",
+                         _ root: String = "",
+                         _ bottom: String = "") -> String {
+        guard let node = node else {
+            return root + "nil\n"
+        }
+        if node.leftChild == nil && node.rightChild == nil {
+            return root + "\(node.value)\n"
+        }
+        return diagram(for: node.rightChild,
+                       top + " ", top + "┌──", top + "│ ")
+    } }
+
+struct BinarySearchTree<Element: Comparable>{
+    
     private(set) var root :BinaryNode<Element>
     
-    init() {
-    }
+    init() {}
 }
 
-extension BinarySeachTree: CustomStringConvertible{
+extension BinarySearchTree: CustomStringConvertible {
     
-    var description : String{
-        guard let root = root else {return "Empty tree"}
-        return String(String(describing: root))
+    var description: String {
+        
+        guard let root = root else { return "Empty tree" }
+        return String(describing: root)
+        
     }
     
 }
+
 
