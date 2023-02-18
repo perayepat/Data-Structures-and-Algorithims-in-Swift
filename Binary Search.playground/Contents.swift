81b369b6073af19ac98d06c0a60e5478d5c60942
Mphathi Maapola <perrierpat@icloud.com>
2023-02-18 11:51:10 +0200

    Binary Search algorithm has been implemented

--- Binary Search.playground/Contents.swift
+++ Binary Search.playground/Contents.swift
@@ -1,4 +1,25 @@
 import UIKit
 
-var greeting = "Hello, playground"
- 
+let numbers = [1,5,15,17,19,22,24,31,105,150]
+var lowerBound = 0
+var upperBound = numbers.count - 1
+var middle = 0
+var found = false
+var searchedValue = 1
+
+while(lowerBound <= upperBound){
+    
+    //find the middle of the array
+    middle = (lowerBound + upperBound) / 2
+    
+    if(numbers[middle] == searchedValue){
+        found = true
+        break
+    } else if(numbers[middle] < searchedValue) {
+       lowerBound = middle + 1
+    } else {
+        upperBound = middle - 1
+    }
+}
+
+print(found)
